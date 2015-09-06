4b4574e3f43a26c44210cde89b00be80f55fed79
lokialice <lokialicehd@gmail.com>
2015-09-06 08:15:35 +0700

    Initial Commit

---
+++ SkeletonApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkeletonApp
+//
+//  Created by Macbook Pro MD102 on 9/6/15.
+//  Copyright (c) 2015 Loki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
